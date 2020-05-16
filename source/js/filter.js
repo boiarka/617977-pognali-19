11a9c5aa70eeeca0323d01f312e8c84022eb78d6
Yurii <yurii.androsiuk@gmail.com>
2020-05-16 05:03:03 +0300

    tooltip fix, js для алфавита в каталоге

--- source/js/filter.js
+++ source/js/filter.js
@@ -6,7 +6,6 @@ const filterCountries = document.querySelector(".country-filter__countries");
 const filterCloseText = document.querySelector(".country-filter__close");
 const filterShowAllText = document.querySelector(".country-filter__show-all");
 
-
 const filterTurnButton = document.querySelector(".country-filter__turn-button");
 
 function filterToggler() {
@@ -21,3 +20,27 @@ function filterToggler() {
 
 filterToggle.addEventListener("click", filterToggler);
 filterTurnButton.addEventListener("click", filterToggler);
+
+
+// test
+
+const countriesListDiv = document.querySelector(".country-filter__countries-checked");
+const alphabetButtons = document.querySelectorAll(".alphabet .alphabet__button");
+const alphabetLists = document.querySelectorAll(".alphabet .alphabet__list");
+
+function pickList(dataset) {
+  for (let k = 0; k < alphabetLists.length; k++) {
+    if (alphabetLists[k].dataset.info == dataset) {
+      alphabetLists[k].classList.remove("alphabet__list");
+      alphabetLists[k].classList.add("country-filter__list", "countries-list", "countries-list--active");
+      countriesListDiv.innerHTML = "";
+      countriesListDiv.appendChild(alphabetLists[k]);
+    }
+  }
+}
+
+for (let i = 0; i < alphabetButtons.length; i++) {
+  alphabetButtons[i].addEventListener("click", function () {
+    pickList(alphabetButtons[i].dataset.info);
+  });
+}
